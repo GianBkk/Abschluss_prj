615e1f3a13634c529ddc315ab1e440412dd64771
GianBkk <gian200308@gmail.com>
2022-01-25 14:53:31 +0100

    initial commit

---
+++ backend/src/data/data.controller.ts
@@ -0,0 +1,30 @@
+import { Controller, Get, Param} from '@nestjs/common';
+import { Data } from 'src/models/data.entity';
+import { DataService } from './data.service';
+
+
+@Controller('data')
+export class DataController {
+    constructor(private readonly dataService: DataService) {}
+
+    @Get('/desc/:id')
+    async getAllByPrinterDESC(@Param('id') printerId : string): Promise<Data[]> {
+        return await this.dataService.getAllByPrinterDESC(printerId);
+    }
+
+    @Get('/all/:id')
+    async getAllByPrinterASC(@Param('id') printerId : string): Promise<Data[]> {
+        return await this.dataService.getAllByPrinterASC(printerId);
+    }
+
+    @Get(':id')
+    async getOneByPrinter(@Param('id') printerId : string): Promise<Data> {
+        return await this.dataService.getOneByPrinter(printerId);
+    }
+
+    @Get()
+    async getAll(): Promise<Data[]> {
+        return await this.dataService.getAllData();
+    }
+
+}
