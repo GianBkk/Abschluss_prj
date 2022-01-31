efd788afcfdcada440d7187e768ead84012e8e6c
GianBkk <gian200308@gmail.com>
2022-01-31 13:37:46 +0100

    minor fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,12 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'http://localhost:8080',
-    methods: 'GET, PUT, POST, DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
   });
+  
   await app.listen(process.env.API_PORT);
 }
 bootstrap();
